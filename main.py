5036b0723c226e278b9bf73c7e89a8bbeb47d1ac
DimitriDomingo <dimitriaugusto65@gmail.com>
2025-10-23 20:41:21 -0300

    Menu interativo adicionado

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Biblioteca:
 
     def adicionar_livro(self, livro):
         self.livros.append(livro)
-        print(f'O livro: {livro} foi adicionado a biblioteca!')
+        print(f'O livro: {livro.titulo} foi adicionado a biblioteca!')
 
     def remover_livro(self,livro):
         self.livros.remove(livro)
@@ -26,7 +26,45 @@ class Biblioteca:
     def listar_livros(self):
         print('Esta é a lista de livros:')
         for livro in self.livros:
-            print(livro.mostrar_livro())
+            livro.mostrar_livro()
 
+def menu_interativo(biblioteca):
+    while True:
+        print('-=' * 15)
+        print('1) Adicionar livros.')
+        print('2) remover livro.')
+        print('3) Listar livros.')
+        print('4) Encerrar.')
+        print('-=' * 15)
 
+        opcao = int(input('Digite o número da operação que deseja fazer: '))
 
+        if opcao == 1:
+            livro = input('Livro: ')
+            autor = input('Autor: ')
+            estilo = input('Estilo: ')
+
+            novo_livro = Livro(livro, autor, estilo)
+            biblioteca.adicionar_livro(novo_livro)
+
+        elif opcao == 2:
+            titulo = input('Livro: ')
+            livro_encontrado = None
+            for livro in biblioteca.livros:
+                if livro.titulo == titulo:
+                    print('Livro encontrado!')
+                    livro_encontrado = titulo
+                    break
+                if livro_encontrado:
+                    biblioteca.remover_livro(livro_encontrado)
+
+            biblioteca.remover_livro()
+
+        elif opcao == 3:
+            biblioteca.listar_livros()
+
+        else:
+            break
+
+biblioteca = Biblioteca()
+menu_interativo(biblioteca)
